8a07818e50a82a9ecf634091545470600babaeba
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-22 16:04:57 +0200

    added empty file

---
+++ Salad/Salad/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Salad
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
