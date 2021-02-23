9353606130c1bbdecd22676dd37ed820ed982d7e
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-02-23 21:25:57 +0200

    bad very bad

---
+++ Salad/Salad/HierarchyofVegetable/GroupofVegetable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salad
+{
+    public class GroupofVegetable : LifeCycleVegetable
+    {
+        public TypeofGroup TypeofGroup { get; set; }
+    }
+}
---
+++ Salad/Salad/HierarchyofVegetable/TypeofGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salad
+{
+    public enum TypeofGroup
+    {
+        Cabbages,
+        RootVegetables,
+        Tubers,
+        Bulbous,
+        Fruits,
+        Leafy
+    }
+}
